03d8fe6bf859d3407f3ea47ad1ea8e0591db95f5
DarGrz <102190066+DarGrz@users.noreply.github.com>
2022-09-06 01:23:18 +0200

    css styling added

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
+import { Fragment } from "react";
 import Head from "next/head";
-import MeetupList from "../components/meetups/MeetupList";
 import { MongoClient } from "mongodb";
+
+import MeetupList from "../components/meetups/MeetupList";
 
 function HomePage(props) {
   return (
-    <>
+    <Fragment>
       <Head>
-        <title>React Places</title>
+        <title>React Meetups</title>
         <meta
           name="description"
-          content="Browse a huge list of highly active React Meetups"
+          content="Browse a huge list of highly active React meetups!"
         />
       </Head>
-      <MeetupList meetups={props.meetups} />
-    </>
+      <MeetupList meetups={props.meetups} />;
+    </Fragment>
   );
 }
 
@@ -32,22 +34,23 @@ function HomePage(props) {
 
 export async function getStaticProps() {
   // fetch data from an API
-
   const client = await MongoClient.connect(
     "mongodb+srv://DarGrz:Anakonda22@cluster0.ek4gwl7.mongodb.net/meetups?retryWrites=true&w=majority"
   );
   const db = client.db();
 
   const meetupsCollection = db.collection("meetups");
+
   const meetups = await meetupsCollection.find().toArray();
+
   client.close();
+
   return {
     props: {
       meetups: meetups.map((meetup) => ({
         title: meetup.title,
         address: meetup.address,
         image: meetup.image,
-        description: meetup.description,
         id: meetup._id.toString(),
       })),
     },
